3676945af3f0079b621550590238b1e9a167798c
Victor-software123 <victor.soft1100@gmail.com>
2025-12-11 16:05:26 -0300

    aula 01 - lógica de programação

---
+++ 2. Aprendizagem/Aula 01/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Main {
+/*  int minhaidade = 20;
+    String meunome = "GABRIEL";
+
+*/
+}
+class tipos_primitivos{
+/*     byte => 8 bits => -128 a 127
+    short => 16 bits => -32.000 a 32.000
+    int => 32 bits => -2.000.000.000 a 2.000.000.000
+    long => 64 bits => -9.000.000.000.000.000.000 a 9.000.000.000.000.000.000
+
+    float => 32 bits => precisão simples pós virgula
+    double => 64 bits => precisão dupla pós virgula
+
+    char => representar um unico caractere => a, b, c, d...
+    char meuChar = 'a';
+    meuChar = "Fernanda"; //Não pode atribuir como string
+
+    Boolean => true ou false
+
+*/
+    public static void main(String[] args) {
+        /*byte b = 100;
+        char c = 'A';
+        double d = 20.5;
+        float f = 10.5f;
+        boolean bool = true;
+
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("victor");
+        lista.add("daniel");
+        lista.add("gabriel");
+
+        lista.remove("daniel");
+
+        System.out.println(lista.get(1));*/
+
+        /*for (int numero = 0; numero < 5; numero++){
+            System.out.print(numero + 1);
+        }
+
+        int contador = 0;
+        while(contador < 10){
+            System.out.println("Ainda em processso");
+            contador++;
+            if(contador == 10){
+                System.out.println("Pronto, terminado e eu tenho");
+            }
+
+        }*/
+
+        double resultado = 0.0;
+        int resultadoInt = (int) resultado;
+
+        String meuString = "10";
+        int meuInt2 = Integer.parseInt(meuString);
+
+    }
+}
+
